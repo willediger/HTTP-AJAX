fc9edb07b1e8fb2038d52bd44f2a0c5fd16a1180
unknown <will.ediger@gmail.com>
2019-04-05 19:41:10 -0500

    styled nav links

--- friends/src/components/Nav.js
+++ friends/src/components/Nav.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import styled from "styled-components";
+
+const FriendsNav = styled.nav`
+  display: flex;
+  width: 250px;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
 function Nav() {
   return (
     <nav>
       <h1>Friends</h1>
-      <div>
+      <FriendsNav>
         <NavLink exact to="/">
           Home
         </NavLink>
         <NavLink to="/friend-list">Friend List</NavLink>
         <NavLink to="/friend-form">Add Friend</NavLink>
-      </div>
+      </FriendsNav>
     </nav>
   );
 }
